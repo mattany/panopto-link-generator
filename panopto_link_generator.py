e36a9d946b8dff5d26e0926c249372d8d7f400cf
Mattan Yeroushalmi <mattany@gmail.com>
2020-04-21 00:10:18 +0300

    Add files via upload

--- panopto_link_generator.py
+++ panopto_link_generator.py
@@ -1,11 +1,10 @@
 import webbrowser
-
 while True:
-    LINK = input("Please paste the link to the Lecture and press enter:")
-    try:
-        url = f"https://huji.cloud.panopto.eu/Panopto/Podcast/Social/{LINK.split('=')[1]}.mp4"
-        webbrowser.open(url, new=0, autoraise=True)
-
-    except:
-        print("bad link. try again.")
+	LINK = input("Please paste the link to the Lecture and press enter:")
+	try:
+		url = f"https://huji.cloud.panopto.eu/Panopto/Podcast/Social/{LINK.split('=')[1]}.mp4"
+		print(url)
+		webbrowser.open(url, new=0, autoraise=True)
+	except:
+		print("bad link. try again.")
 
